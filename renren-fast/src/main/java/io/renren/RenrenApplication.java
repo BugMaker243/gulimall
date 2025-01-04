496c634107c68357f9641ee3326b8c49723faf43
BugMaker243 <2022217471@mail.hfut.edu.cn>
2025-01-04 23:00:25 +0800

    添加renren开源手脚架，已经加好了人人后端的项目，并且把依赖相关的东西配置后了，刚刚引入renren代码生成器，什么都没有改，先git一下

---
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -0,0 +1,21 @@
+package io.renren;
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RenrenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenrenApplication.class, args);
+	}
+
+}
